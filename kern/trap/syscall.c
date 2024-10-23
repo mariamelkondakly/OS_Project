4cc5cb3ef4b5e298b8397815bde9d8fb569c0252
mariamelkondakly <mariamelkondakly88@gmail.com>
2024-10-23 14:13:26 +0300

    updates

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -302,6 +302,10 @@ int sys_pf_calculate_allocated_pages(void)
 /*******************************/
 void sys_free_user_mem(uint32 virtual_address, uint32 size)
 {
+	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
+	if((uint32)virtual_address==0||(uint32)virtual_address<USER_HEAP_START||(uint32)virtual_address>USER_HEAP_MAX){
+			env_exit();
+	}
 	if(isBufferingEnabled())
 	{
 		__free_user_mem_with_buffering(cur_env, virtual_address, size);
@@ -325,10 +329,6 @@ void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 
 void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 {
-	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
-	if((uint32)virtual_address==0||(uint32)virtual_address<USER_HEAP_START||(uint32)virtual_address>USER_HEAP_MAX){
-			env_exit();
-		}
 	allocate_chunk(cur_env->env_page_directory, virtual_address, size, perms);
 	return;
 }
